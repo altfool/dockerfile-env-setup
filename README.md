d02ca8fc32e2f39d0f6e88a81f81536cc91da098
Zack Yang <zongmiy@clemson.edu>
2021-01-03 15:58:33 -0500

    add u16,u18 files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dockerfile-env-setup
 used to setup ubuntu+ros environment and enable display setting and shared folder
+
+Please use u18-ros-melodic.sh to launch ubuntu18+ros-melodic;
+Please use u16-ros-kinetic.sh to launch ubuntu16+ros-kinetic;
+
+Or you may specify your own docker image version;
+The shared folder location is: $HOME/shared_dir in host system and /root/shared_dir in docker container;
+
+Please use "docker start $NAME" to launch this container. 
+And use "docker exec -it $NAME bash" to interactive with this container. 
+After usage, please use "docker stop $NAME" to stop this container.
+Please use "docker rm $NAME" to remove this container.
---
+++ u18-ros-melodic.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+# Default Settings
+NAME="u18-melodic"
+CUDA="on"  # "on" or "off"
+IMAGE_NAME="ros"
+IMAGE_TAG="melodic-robot"
+IMAGE=$IMAGE_NAME:$IMAGE_TAG
+echo -e "ROS distro: $IMAGE"
+
+RUNTIME=""
+
+XSOCK=/tmp/.X11-unix
+XAUTH=$HOME/.Xauthority
+
+SHARED_DOCKER_DIR=/root/shared_dir
+SHARED_HOST_DIR=$HOME/shared_dir
+
+VOLUMES="--volume=$XSOCK:$XSOCK:rw
+         --volume=$XAUTH:$XAUTH:rw
+         --volume=$SHARED_HOST_DIR:$SHARED_DOCKER_DIR:rw"
+
+DOCKER_VERSION=$(docker version --format '{{.Client.Version}}' | cut --delimiter=. --fields=1,2)
+if [ $CUDA == "on" ]; then
+    if [[ ! $DOCKER_VERSION < "19.03" ]] && ! type nvidia-docker; then
+        RUNTIME="--gpus all"
+    else
+        RUNTIME="--runtime=nvidia"
+    fi
+fi
+
+# Create the shared directory in advance to ensure it is owned by the host user
+mkdir -p $SHARED_HOST_DIR
+
+if [ "$(docker ps -aq -f name=$NAME)" ]; then
+    echo -e "\n***Container $NAME exists.***\n\nPlease use \"docker start $NAME\" to launch this container. And use \"docker exec -it $NAME bash\" to interactive with this container. After usage, please use \"docker stop $NAME\" to stop this container."
+    echo -e "Or you may use \"docker rm $NAME\" to remove this container and re-run this script to create the container again."
+    # docker rm $NAME
+else
+    echo "Launching $IMAGE"
+    docker run \
+        -it \
+        $VOLUMES \
+        --env="XAUTHORITY=${XAUTH}" \
+        --env="DISPLAY=${DISPLAY}" \
+        --privileged \
+        --net=host \
+        --name=$NAME \
+        $RUNTIME \
+        $IMAGE
+fi
