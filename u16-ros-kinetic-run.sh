ba7b6f494e92064f949aec553691432a33d5b86d
Zack Yang <zongmiy@clemson.edu>
2021-01-04 14:49:46 -0500

    modify Xauth file creation

--- u16-ros-kinetic-run.sh
+++ u16-ros-kinetic-run.sh
@@ -21,12 +21,13 @@ XAUTH=/tmp/.docker.xauth
 echo "Preparing Xauthority data..."
 xauth_list=$(xauth nlist :0 | tail -n 1 | sed -e 's/^..../ffff/')
 if [ ! -f $XAUTH ]; then
-    if [ ! -z "$xauth_list" ]; then
-        echo $xauth_list | xauth -f $XAUTH nmerge -
-    else
-        touch $XAUTH
-    fi
+    echo -e "$XAUTH not exists. Create it now."
+    touch $XAUTH
     chmod a+r $XAUTH
+fi
+if [ ! -z "$xauth_list" ]; then
+    echo -e "merge xauth list to file $XAUTH"
+    echo $xauth_list | xauth -f $XAUTH nmerge -
 fi
 echo "Done."
 echo -e "\nVerifying file contents:"
